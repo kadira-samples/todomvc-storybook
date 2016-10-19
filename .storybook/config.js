ba7bec0f72bddddd5cfff2e34833a32ffbc3a958
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-10-19 19:42:47 +0530

    Using require.context to load story files.

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,8 +2,10 @@ import { configure } from '@kadira/storybook';
 
 import 'todomvc-app-css/index.css';
 
+const req = require.context('../src/components', true, /\.stories\.js$/);
+
 function loadStories() {
-  require('../src/stories');
+  req.keys().forEach(path => req(path));
 }
 
 configure(loadStories, module);
--- src/stories/index.js
+++
@@ -1,1 +0,0 @@
-import './MainSection';

17aaf2d3badea5e0fbc23adec2984c432ed79904
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-10-25 18:14:47 +0530

    Setup the storybook.

--- src/stories/MainSection.js
+++ src/stories/MainSection.js
@@ -2,26 +2,15 @@ import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import MainSection from '../components/MainSection';
 
-const stories = storiesOf('MainSection', module);
+const stories = storiesOf('Knobs', module);
 
 stories.add('with a single todo item', () => (
   <MainSection
-    todos={[{id: '1', text: 'Learn React'}]}
+    todos={[{
+      id: '1',
+      text: 'Learn React',
+      completed: false,
+    }]}
     actions={{}}
   />
-))
-
-stories.add('with a few todo items', () => {
-  const todos = [
-    {id: '1', text: 'Learn React', completed: true},
-    {id: '2', text: 'Use CRA', completed: true},
-    {id: '3', text: 'Try React Storybook'},
-  ];
-
-  return (
-    <MainSection
-      todos={todos}
-      actions={{}}
-    />
-  );
-});
+));
